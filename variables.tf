f0e3a86386b06c6472680b51d7a6fe9784bf34fd
hyper11011 <120276273+hyper11011@users.noreply.github.com>
2026-03-27 23:43:38 +0200

    chore(aware): enable inspector.amazonaws.com for operational efficiency (x3)
    Add inspector.amazonaws.com to enable_aws_services default list to enable
    automated vulnerability scanning across all organization accounts. This
    improves operational visibility and security posture detection.
    
    Note: delegation.tf already supports Inspector delegation via
    enable_delegation.inspection variable, but the service was not enabled.

--- variables.tf
+++ variables.tf
@@ -1,4 +1,3 @@
-
 variable "tags" {
   description = "A map of tags to resources provisioned by this module."
   type        = map(string)
@@ -66,6 +65,7 @@ variable "enable_aws_services" {
     "controltower.amazonaws.com",
     "cost-optimization-hub.bcm.amazonaws.com",
     "guardduty.amazonaws.com",
+    "inspector.amazonaws.com",
     "ram.amazonaws.com",
     "securityhub.amazonaws.com",
     "servicequotas.amazonaws.com",
@@ -221,7 +221,7 @@ variable "enable_delegation" {
       account_id = string
     }), null)
     cloudtrail = optional(object({
-      # The id of the account to delegate the management of CloudTrail to
+      # The id of the account to delegate the CloudTrail to
       account_id = string
     }), null)
     guardduty = optional(object({
